8dd02d83e74f71ef440824d19e3e5aff4c28ddbe
dankernel <google@dankook.ac.kr>
2015-01-06 05:07:03 +0900

    auto-20150106-050703

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 gcc -finput-charset=UTF-8  -D__KERNEL__ -pg -g -lm -O4 -o main main.c
 ctags -R --exclude=dox
 # ./main data/bit.csv 16
-# ./main data/hm_1.csv 8
-./main data/proj_4.csv 2
+./main data/hm_1.csv 1
+# ./main data/proj_4.csv 2
